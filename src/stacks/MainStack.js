29844d47324e88e3f64e660982b39a9d16094f4c
sadrakmorais <sadraksdk1@gmail.com>
2021-09-04 12:43:19 -0300

    Att

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -6,7 +6,9 @@ import SignIn from '../screens/SignIn'
 import SignUp from '../screens/SignUp'
 const Stack = createStackNavigator()
 export default () =>(
-  <Stack.Navigator>
+  <Stack.Navigator  screenOptions={{
+    headerShown:false
+  }}>
       <Stack.Screen name="Preload" component ={Preload}/>
       <Stack.Screen name="SignIn" component ={SignIn}/>
       <Stack.Screen name="SignUp" component ={SignUp}/>
